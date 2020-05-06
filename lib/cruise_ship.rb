673c3e5189c55350419595e45019b66ecc384b6c
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 22:29:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-
+  winner = ""
+  passenger.each do |suite, name| 
+    if suite === :suite_a && name.start_with?("A")
+      winner = name 
+    end 
 end
+  winner 
+end 
